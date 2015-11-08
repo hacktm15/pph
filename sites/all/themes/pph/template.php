f4c8e91153167c018c0ac820710b1c9f6b1579e1
Emanuel Greucean <emanuel@greucean.me>
2015-11-08 16:40:49 +0200

    Improve styling for specialties and location

--- sites/all/themes/pph/template.php
+++ sites/all/themes/pph/template.php
@@ -1,5 +1,24 @@
-<?php 
-/** 
- * @file 
+<?php
+/**
+ * @file
  * Primary pre/preprocess functions and alterations.
- */ 
+ */
+
+/**
+ * Theme preprocess function for a location.
+ */
+function pph_preprocess_location(&$variables) {
+  $location = array();
+  if (!empty($variables['city'])) {
+    $location[] = $variables['city'];
+  }
+  if (!empty($variables['province'])) {
+    $location[] = $variables['province'];
+  }
+  if (!empty($variables['country_name'])) {
+    $location[] = $variables['country_name'];
+  }
+  if (!empty($location)) {
+    $variables['location_processed'] = implode(', ', $location);
+  }
+}
---
+++ sites/all/themes/pph/templates/location.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Template for displaying single location.
+ */
+?>
+<div class="location">
+  <?php if (!empty($location_processed)) print $location_processed; ?>
+</div>
+
